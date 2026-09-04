893a765cd1df58591bd02388e7c9ee15c7c18b95
barracharles-glitch <barra.charles@student.ccm.edu>
2026-09-04 14:25:51 -0400

    Implement store sales input functionality
    Added user input for the number of stores and sales entry.

--- Patterns.java
+++ Patterns.java
@@ -1,5 +1,20 @@
 package Labs.Week_1.scr;
+import java.util.Scanner;
 
 public class Patterns {
-    
+    public static void main(String args []) {
+        int stores;
+        
+
+        Scanner int_input = new Scanner(System.in);
+        System.out.print("How many stores are there?: ");
+        stores = int_input.nextInt();
+
+        for(int i=0; i < stores; i++){
+            System.out.print("Enter today's sales for store " + (i+1) + ": ");
+            
+        }
+
+        int_input.close();
+    }
 }
