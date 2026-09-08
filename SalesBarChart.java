2e6a589d172b6ec94833fe3db0d646539d479fee
barracharles-glitch <barra.charles@student.ccm.edu>
2026-09-08 14:44:14 -0400

    9/8/2026

--- SalesBarChart.java
+++ SalesBarChart.java
@@ -1,5 +1,21 @@
 package Labs.Week_1.scr;
 
 public class SalesBarChart {
-    
+    public static void main(String args []){
+        int ast = 1;
+        for (int j = 0; j < 10; j++){
+            for (int i = 0; i < ast; i++){
+                System.out.print("*");
+            }
+            System.out.println();
+            ast += 1;
+        for (int j = 0; j < 10; j++){
+            for (int i = 0; i < ast; i++){
+                System.out.print("*");
+            }
+            System.out.println();
+            ast += 1;
+            
+        }
+    }
 }
